fea081f9e8a8314fe08969a61ff6e3ec836fa145
BogdanMalashuk <kufar.120@gmail.com>
2024-11-27 23:12:06 +0300

    Первое задание

--- Home8/first_task.py
+++ Home8/first_task.py
@@ -5,32 +5,33 @@
 попадания в тот или иной диапазон. Использовать обработку
 исключений.
 """
+while True:
+    try:
+        user_weight = float(input("Enter weight (kg): "))
+        user_height = float(input("Enter height (m): "))
+        imt = user_weight / (user_height ** 2)
 
-try:
-    user_weight = float(input("Enter weight (kg): "))
-    user_height = float(input("Enter height (m): "))
-    imt = user_weight / (user_height ** 2)
+        if imt <= 0:
+            raise ValueError
 
-    if imt <= 0:
-        raise ValueError
+        if imt < 16.5:
+            user_imt_catg = "Super low weight!"
+        elif 16.5 < imt < 18.5:
+            user_imt_catg = "Low weight"
+        elif 18.5 <= imt < 24.9:
+            user_imt_catg = "Normal weight"
+        elif 25 <= imt < 29.9:
+            user_imt_catg = "Light overweight"
+        elif 30 <= imt < 34.9:
+            user_imt_catg = "Overweight, 1 step!"
+        elif 35 <= imt < 39.9:
+            user_imt_catg = "Overweight, 2 step!"
+        else:
+            user_imt_catg = "Overweight, 3 step or more!"
 
-    if imt < 16.5:
-        user_imt_catg = "Super low weight!"
-    elif 16.5 < imt < 18.5:
-        user_imt_catg = "Low weight"
-    elif 18.5 <= imt < 24.9:
-        user_imt_catg = "Normal weight"
-    elif 25 <= imt < 29.9:
-        user_imt_catg = "Light overweight"
-    elif 30 <= imt < 34.9:
-        user_imt_catg = "Overweight, 1 step!"
-    elif 35 <= imt < 39.9:
-        user_imt_catg = "Overweight, 2 step!"
-    else:
-        user_imt_catg = "Overweight, 3 step or more!"
+        print(f"Body weight index: {round(imt, 2)}\n"
+              f"Index category: {user_imt_catg, 1}")
+        break
 
-    print(f"Body weight index: {imt}\n"
-          f"Index category: {user_imt_catg}")
-
-except ValueError:
-    print(f"Exception. Enter correct value")
+    except ValueError:
+        print(f"Exception. Enter correct value")
