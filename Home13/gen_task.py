447a1a203bf72d07c36979cf514e69222f30f817
BogdanMalashuk <kufar.120@gmail.com>
2024-12-16 21:03:56 +0300

    Чтение файла + генератор

---
+++ Home13/gen_task.py
@@ -0,0 +1,36 @@
+"""
+Задача: Реализуйте генератор, который построчно считывает текстовый файл (ленивое чтение).
+Напишите функцию file_reader(file_path), которая возвращает строки файла по одной.
+Не загружайте весь файл в память.
+Пример использования:
+python
+Копировать код
+for line in file_reader("example.txt"):
+    print(line.strip())
+Дополнительно: Реализуйте генератор, который возвращает только строки, содержащие слово "Python".
+
+"""
+
+
+def reader(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            yield line.strip()
+
+
+def generator(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in reader(file_path):
+            if 'python' in line.lower():
+                yield line.strip()
+
+
+text_file = "text.txt"
+
+print("file content:")
+for line in reader(text_file):
+    print(line)
+
+print("\n\nstrings with 'python':")
+for line in generator(text_file):
+    print(line)
